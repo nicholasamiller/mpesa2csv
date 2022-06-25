b38a086daf6ca98d0049486e500ab40c731361f2
Nicholas Andrew Miller <nick.miller@govlawtech.com.au>
2022-06-25 16:30:35 +1000

    part way through adding blazor front end...

---
+++ MPesa2Csv/MPesa2Csv.Conversion/RowRecord.cs
@@ -0,0 +1,33 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace MPesa2Csv
+{
+    record RowRecord
+    {
+
+        [Name("Recipt Number")]
+        public string ReciptNo { get; set; }
+
+        [Name("Completion Time (Kenya/Nairobi)")]
+        public DateTime CompletionTime { get; set; }
+
+        [Name("Details")]
+        public string Details { get; set; }
+
+        [Name("Transaction Status")]
+        public string TransactionStatus { get; set; }
+
+        [Name("Paid In")]
+        public decimal PaidIn { get; set; }
+
+
+        [Name("Withdrawn")]
+        public decimal WithDrawn { get; set; }
+
+
+        [Name("Balance")]
+        public decimal Balance { get; set; }
+
+
+    }
+}
